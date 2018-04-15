82c1a296129cceb18ab8f2a7416c594ed19ccc33
Loïs Bilat <lois.bilat@gmail.com>
2018-04-15 17:48:00 +0200

    Commented code from lab2

--- Lab2/filters/thresmain.c
+++ Lab2/filters/thresmain.c
@@ -46,26 +46,32 @@ int main (int argc, char ** argv) {
     pthread_mutex_t lock;
     pthread_mutex_init(&lock, NULL);
 
+    // Gobal value use to compute the average value of pixels
     int sum = 0;
+
+    // Create all threads
     for (int i = 0; i < nthreads; ++i)
     {
+        // Decide what each thread will work on
 		int num_rows_per_job = ceil((float)ysize/(float)nthreads);
 		int from_row = (i*num_rows_per_job);
 		int to_row = from_row + num_rows_per_job < ysize ? from_row + num_rows_per_job : ysize;
 		
+        // Give specific data to each thread
         thread_datas[i] = (thread_data){xsize, ysize, from_row, to_row, &sum, src, &barr, &lock};
 		
+         // Create the thread itself
 		pthread_create(threads + i, NULL, thresfilter, thread_datas + i);
 		
 	}
+    // We now wait until all threads are done executing, using join
 	for (int i = 0; i < nthreads; ++i)
 	{
 		pthread_join(threads[i], NULL);
 	}
     
+    // Don't need the barrier anymore
 	pthread_barrier_destroy(&barr);
-
-
 
     clock_gettime(CLOCK_REALTIME, &etime);
 
@@ -78,7 +84,6 @@ int main (int argc, char ** argv) {
     
     if(write_ppm (argv[2], xsize, ysize, (char *)src) != 0)
       exit(1);
-
 
     return(0);
 }
