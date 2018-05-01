b2508525846bf40c3eecfa9c14c0bdca40a96665
Loïs Bilat <x_loibi@triolith1.(none)>
2018-05-01 12:24:54 +0200

    New plots

--- Lab3/eval_lab3.py
+++ Lab3/eval_lab3.py
@@ -9,7 +9,7 @@ from subprocess import check_output
 num_jobs = range(0, 5)  
 num_jobs = [2**i for i in num_jobs]
 
-grid_size = [1000, 2000, 3000, 5000, 10000]  
+grid_size = [500, 1000, 2000, 4000, 8000, 16000]  
 
 time_all = np.zeros((len(num_jobs), len(grid_size)))
 
